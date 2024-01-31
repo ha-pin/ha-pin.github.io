accad9b5f475f6c066229ae432cb5ee82e36639d
HerbertHe <Herbert.He0229@gmail.com>
2024-01-31 11:50:31 +0800

    fix :bug: : update

--- src/playground/playground.tsx
+++ src/playground/playground.tsx
@@ -6,10 +6,16 @@ const words = {
   'zh-CN': {
     title: '哈拼演练场',
     desc: '哈拼演练场是为初学者准备的直观练习',
+    hapin: '哈拼方案',
+    arabic: '老文字',
+    cyrillic: '西里尔字母',
   },
   'en-US': {
     title: 'Hapin Playground',
     desc: '',
+    hapin: 'Hapin Scheme',
+    arabic: 'The Persian-Arabic Alphabet',
+    cyrillic: 'The Cyrillic Alphabet',
   },
 };
 
@@ -20,94 +26,109 @@ const qwerty_keyboard = [
 ];
 
 // TODO 映射
-const hapin_mapping = new Map([['', []]]);
+const hapin_maping = new Map([
+  ['a', ['ا', 'А а']],
+  ['b', ['ب', 'Б б']],
+  ['d', ['د', 'Д д']],
+  ['e', ['ى', 'Ы ы']],
+  ['f', ['ف', 'Ф ф']],
+  ['g', ['گ', 'Г г']],
+  ['h', ['ح', 'Х х']],
+  ['i', ['ي', 'Й й И и']],
+  ['j', ['ج', 'Ж ж']],
+  ['k', ['ك', 'К к']],
+  ['l', ['ل', 'Л л']],
+  ['m', ['م', 'М м']],
+  ['n', ['ن', 'Н н']],
+  ['o', ['و', 'О о']],
+  ['p', ['پ', 'П п']],
+  ['q', ['ق', 'Қ қ']],
+  ['r', ['ر', 'Р р']],
+  ['s', ['س', 'С с']],
+  ['t', ['ت', 'Т т']],
+  ['u', ['ۇ', 'Ұ ұ']],
+  ['v', ['ۆ', 'В в']],
+  ['w', ['ۋ', 'У у']],
+  ['z', ['ز', 'З з']],
+  ['ye', ['ە', 'Е е']],
+  ['gh', ['ع', 'Ғ ғ']],
+  ['hh', ['ھ', 'Һ һ']],
+  ['ng', ['ڭ', 'Ң ң']],
+  ['sh', ['ش', 'Ш ш']],
+  ['ch', ['چ', 'Ч ч']],
+  ['xa', ['ٴا ', 'Ә ә']],
+  ['xe', ['ٴى ', 'І і']],
+  ['xo', ['ٴو', 'Ө ө']],
+  ['xu', ['ٴۇ', 'Ү ү']],
+  ['yu', ['ٴۇ', 'Ү ү']],
+  [',', ['،', ',']],
+  ['?', ['؟', '?']],
+  [';', ['	؛', ';']],
+]);
+
+const double_chars = ['x', 'y', 'g', 'h', 'n', 's', 'c'];
+
+const double_valid = [
+  'ye',
+  'gh',
+  'hh',
+  'ng',
+  'sh',
+  'ch',
+  'xa',
+  'xe',
+  'xo',
+  'xu',
+  'yu',
+];
 
 export default function Playground() {
   // ! 改成 before 更简单逻辑实现
+  const [before, setBefore] = useState<string>();
   const [hapin, setHapin] = useState<string>();
   const [lang, setLang] = useState<string>('en-US');
-  const [double, setDouble] = useState<string>();
+  const [maped, setMaped] = useState<string[]>([]);
 
   useEffect(() => {
+    // TODO 支持更多的语言
     if (location.pathname.split('/')[1] === 'zh-CN') {
       setLang('zh-CN');
     } else {
       setLang('en-US');
     }
 
-    const double_chars = ['x', 'y', 'g', 'h', 'n', 's', 'c'];
-    const double_valid = [
-      'ye',
-      'gh',
-      'hh',
-      'ng',
-      'sh',
-      'ch',
-      'xa',
-      'xe',
-      'xo',
-      'xu',
-      'yu',
-    ];
-
-    window.addEventListener('keydown', (ev) => {
+    window.addEventListener('keypress', (ev) => {
       const { key } = ev;
+      const _hapin = window._hapin;
 
-      if (key === '/') {
-        document.getElementById(`key-?`)?.classList.remove('key-active');
-      }
-
-      // ! 单双冲突的问题
       if (/^[a-zA-Z,;\?]{1}$/.test(key)) {
-        if (!double_chars.includes(key)) {
-          if (!!double) {
-            if (double_valid.includes(`${double}${key}`)) {
-              // 双字符模式
-              setHapin(`${double}${key}`);
-              activeKey(key);
-            } else {
-              // 取消双字符模式
-              deactiveKey(double);
-              setDouble('');
-              setHapin(key);
-              activeKey(key);
-            }
-          } else {
-            // 纯单字符模式
-            setHapin(key);
-            activeKey(key);
-          }
+        if (
+          _hapin?.length === 1 &&
+          double_chars.includes(_hapin) &&
+          double_valid.includes(`${_hapin}${key}`)
+        ) {
+          setHapin(`${_hapin}${key}`);
         } else {
-          // 双字符模式
-          if (!!double) {
-            if (double_valid.includes(`${double}${key}`)) {
-              // 双字符模式
-              setHapin(`${double}${key}`);
-              activeKey(key);
-            } else {
-              // 取消双字符模式
-              deactiveKey(double);
-              setDouble(key);
-              setHapin(key);
-              activeKey(key);
-            }
-          } else {
-            // 不存在序列
-            setDouble(key);
-            activeKey(key);
-          }
+          // 否则直接渲染单参数，并且清空 before
+          // ! 需要修复
+          setBefore(hapin);
+          // deactiveKey(before)
+          setHapin(key);
+          activeKey(key);
         }
       }
     });
-
-    window.addEventListener('keyup', (ev) => {
-      const { key } = ev;
-      if (/^[a-zA-Z,;\?]{1}$/.test(key)) {
-        setHapin(key);
-        document.getElementById(`key-${key}`)?.classList.remove('key-active');
-      }
-    });
   }, []);
+
+  useEffect(() => {
+    window._hapin = hapin;
+    if (!!hapin && hapin_maping.has(hapin)) {
+      setMaped(hapin_maping.get(hapin) as string[]);
+      hapin.split('').forEach((k) => activeKey(k));
+    } else {
+      setMaped([]);
+    }
+  }, [hapin]);
 
   const activeKey = (k: string) => {
     document.getElementById(`key-${k}`)?.classList.add('key-active');
@@ -136,13 +157,14 @@ export default function Playground() {
       </div>
       <div>
         <p>
-          当前激活哈拼方案为: <code className="hapin">{hapin}</code>
+          {words[lang].hapin}: <code className="res-hapin">{hapin}</code>
         </p>
         <p>
-          对应老文字: <code className="arabic"></code>
+          {words[lang].arabic}: <code className="res-arabic">{maped[0]}</code>
         </p>
         <p>
-          对应西里尔字母: <code className="cyrillic"></code>
+          {words[lang].cyrillic}:{' '}
+          <code className="res-cyrillic">{maped[1]}</code>
         </p>
       </div>
     </div>
