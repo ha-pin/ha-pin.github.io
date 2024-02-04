c4fadb92d594cc71e59a9a8b5139d85cc14ea272
HerbertHe <Herbert.He0229@gmail.com>
2024-02-04 17:07:45 +0800

    docs :pencil: : update

--- src/playground/playground.tsx
+++ src/playground/playground.tsx
@@ -10,7 +10,7 @@ const words = {
     arabic: '老文字字母',
     cyrillic: '西里尔字母',
     x: 'X 模式: 哈拼在借鉴老文字对于弱音符号的处理上创造了 X 模式, 例如: xa 相当于是在 a 添加了弱音符号修饰符。支持的修饰的字母仅有: xa, xe, xo, xu 构成与老文字一致。另外为了对西里尔字母的兼容, xx 表示 Ъ ъ, xy 表示 Ь ь',
-    y: 'Y 模式: 哈拼为了支持汉语同音的 ye 和哈萨克斯坦现行西里尔字母, 创造了 Y 模式。这也就意味着, 除了输入音 ye 之外, 其余 Y 模式字母全都为西里尔哈萨克语特有字母, 仅有: yi, yo, yw, ya',
+    y: 'Y 模式: 哈拼为了支持汉语同音的 ye、yu (同 xu) 和哈萨克斯坦现行西里尔字母, 创造了 Y 模式。这也就意味着, 除了输入音 ye、yu (同 xu) 之外, 其余 Y 模式字母全都为西里尔哈萨克语特有字母, 仅有: yi, yo, yw, ya',
   },
   'en-US': {
     title: 'Hapin Playground',
@@ -18,7 +18,7 @@ const words = {
     hapin: 'Hapin Scheme',
     arabic: 'The Persian-Arabic Alphabet',
     cyrillic: 'The Cyrillic Alphabet',
-    x: 'X Mode:',
+    x: 'X Mode: Hapin has created X mode based on how to handle the weak tone with Arabic Alphabet, ex: xa means that we add weak tone symbol to the alphabet a.',
     y: 'Y Mode:',
   },
 };
